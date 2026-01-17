24c94f0215a65c846b0c045c860a8770d9ae5e9f
Kabiraj Khatri <Kabirajkhatri42@gmail.com>
2026-01-17 11:32:03 +1100

    Add basic css to signup page

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import '../styles/SignupPage.css'
 
 function SignUpPage() {
     const [fullName, setFullName] = useState('')
@@ -6,35 +7,38 @@ function SignUpPage() {
     const [password, setPassword] = useState('')
 
     return (
-        <div>
-            <form>
-                <label htmlFor="fullName"> Full Name </label>
-                <input
-                    id= "fullName"
-                    value={fullName}
-                    placeholder="Full Name"
-                    onChange = {(e) => setFullName(e.target.value)}
-                />
-                <label htmlFor="email"> Email Address </label>
-                <input 
-                    id="email"
-                    type="email"
-                    value={email}
-                    placeholder="Email address"
-                    onChange = {(e) => setEmail(e.target.value)}
+        <div className="signup-page">
+            <div className="signup-container">
+                <form>
+                    <label htmlFor="fullName"> Full Name </label>
+                    <input
+                        id= "fullName"
+                        value={fullName}
+                        placeholder="Full Name"
+                        onChange = {(e) => setFullName(e.target.value)}
+                    />
+                    <label htmlFor="email"> Email Address </label>
+                    <input 
+                        id="email"
+                        type="email"
+                        value={email}
+                        placeholder="Email address"
+                        onChange = {(e) => setEmail(e.target.value)}
 
-                />
-                <label htmlFor="password"> Password </label>
-                <input 
-                    id="password"
-                    type="password"
-                    value={password}
-                    placeholder="Password"
-                    onChange = {(e) => setPassword(e.target.value)}
-                />
+                    />
+                    <label htmlFor="password"> Password </label>
+                    <input 
+                        id="password"
+                        type="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange = {(e) => setPassword(e.target.value)}
+                    />
 
-                <button type="submit">Sign up</button>
-            </form>
+                    <button type="submit">Sign up</button>
+                </form>
+            </div>
+            <div className="sigup-photo-section"></div>
         </div>
     )
 }
